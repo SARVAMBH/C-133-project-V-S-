dfd958a8e5b58fc552cc0eb310d91a29fc85b7eb
SARVAMBH <83635572+SARVAMBH@users.noreply.github.com>
2022-08-13 13:09:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function  AC() {
+   window.location = "AC.html";
+}
+
+function CUPBOARD() {
+   window.location = "cupboard.html";
+}
+
+function dining_table() {
+   window.location = "dining table.html";
+}
+
+function fan() {
+   window.location = "fan.html";
+}
+
+function microwave() {
+   window.location = "microwave.html";
+}
+
+function refrigerator() {
+   window.location = "refrigerator.html";
+}
+
+function shelf() {
+   window.location = "shelf.html";
+}
+
+function sofa() {
+   window.location = "sofa.html";
+}
+
+function tv() {
+   window.location = "TV.html";
+}
+
+function washing_machine() {
+   window.location = "washing machine.html";
+}
